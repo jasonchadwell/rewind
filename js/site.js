3428792f9668092b8f151ee452250e25b874ff14
jasonchadwell <chadwell.jason@yahoo.com>
2023-03-05 13:16:51 -0500

    Initial

--- js/site.js
+++ js/site.js
@@ -1,4 +1,42 @@
-//Call Hello World
-function HelloWorld(){
-    alert("Hello World");
+//Get the string from the user entered data on the page
+//Controller function
+function getValue(){
+
+    document.getElementById("alert").classList.add("invisible");
+
+    let userString = document.getElementById("userString").value;
+    let revString = reverseString(userString);
+    displayString(revString);
+
 }
+
+
+//Reverse the string
+//Logic function
+function reverseString(userString){
+
+    let revString = [];
+
+    //Reverse a string using a for loop
+    for (let index = userString.length - 1; index >=0 ; index--) {
+
+        revString += userString[index];
+        
+    }
+
+    return revString;
+
+
+}
+
+
+//Display the reversed string to the user
+//View function
+function displayString(revString){
+
+    //write the message to the page
+    document.getElementById("msg").innerHTML = `Your string reversed is: ${revString}`;
+    //turn on the alert box
+    document.getElementById("alert").classList.remove("invisible");
+}
+
